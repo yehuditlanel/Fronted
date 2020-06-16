c306f032eb2d7570c7284c60668d8178f2310955
yehudit lanel <yehuditlanel@gmail.com>
2020-06-16 14:35:46 +0300

    add edit travel component

--- src/app/components/travels/edit-travel/edit-travel.component.ts
+++ src/app/components/travels/edit-travel/edit-travel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServerService } from '@app/service/server.service';
+import { Travel } from '@app/classes/Travel';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-travel',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-travel.component.css']
 })
 export class EditTravelComponent implements OnInit {
+  travel:Travel=new Travel(null,null,null,null,null,null,null,null);
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,private server:ServerService,private location:Location) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      params=>{
+        this.travel.GroupName=params.GroupName
+        this.travel.CollectionOrDispersing=params.CollectionOrDispersing
+        this.travel.DateOfBegin=params.DateOfBegin
+        this.travel.DateOfEnd=params.DateOfEnd
+        this.travel.DestinationOrSource=params.DestinationOrSource
+        this.travel.Frequency=params.Frequency
+        this.travel.Hour=params.Hour
+        this.travel.TravelCode=params.TravelCode
+        console.log(this.travel);
+      });
+  }
+  onSubmitForm(){
+    this.server.updateObject("Travel",this.travel);
+    this.location.back();   
+  }
+  cancel(){
+    this.location.back();
   }
 
 }
