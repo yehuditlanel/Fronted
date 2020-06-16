555c27a357c7a0a37e94e08ecdc47903873765b9
yehudit lanel <yehuditlanel@gmail.com>
2020-06-16 14:53:17 +0300

    change the view travel

--- src/app/classes/Travel.ts
+++ src/app/classes/Travel.ts
@@ -2,14 +2,16 @@ import { Time } from '@angular/common';
 
 export class Travel{
     TravelCode:number;
+    GroupName:string;
     CollectionOrDispersing:string;
     DestinationOrSource:string;
-    Hour:Time;
+    Hour:string;
     Frequency:string;
     DateOfBegin:Date;
     DateOfEnd:Date;
-    constructor(travelCode:number,collectionOrDispersing:string,destinationOrSource:string,hour:Time,frequency:string,dateOfBegin:Date,dateOfEnd:Date){
+    constructor(travelCode:number,GroupName:string,collectionOrDispersing:string,destinationOrSource:string,hour:string,frequency:string,dateOfBegin:Date,dateOfEnd:Date){
        this.TravelCode=travelCode;
+       this.GroupName=GroupName;
        this.CollectionOrDispersing=collectionOrDispersing;
        this.DestinationOrSource=destinationOrSource;
        this.Hour=hour;
--- src/app/components/travels/all-travels/all-travels.component.ts
+++ src/app/components/travels/all-travels/all-travels.component.ts
@@ -1,40 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Travel } from 'src/app/classes/Travel';
 import { Track } from 'src/app/classes/Track';
 import { ServerService } from 'src/app/service/server.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatTableDataSource } from '@angular/material';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-all-travels',
   templateUrl: './all-travels.component.html',
-  styleUrls: ['./all-travels.component.css']
+  styleUrls: ['./all-travels.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AllTravelsComponent implements OnInit {
-travels:Travel[];
-tracks:Track[];
-  constructor(private server:ServerService,private router:Router) { }
-
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  travels:MatTableDataSource<Travel>;
+  columnsToDisplay = ['GroupName','CollectionOrDispersing','DestinationOrSource','Hour','Frequency','DateOfBegin','DateOfEnd','action'];
+  expandedElement: Travel | null;
+  constructor(private server:ServerService,private router:Router,private route:ActivatedRoute){}
   ngOnInit() {
-    this.travels=[];
-    this.server.byGet("Travel").subscribe(data => {
-    this.travels=data;
-    console.log(this.travels);
-  });
-  this.tracks=[];
-    this.server.byGet("Track").subscribe(data => {
-    this.tracks=data;
-    console.log(this.tracks);
+    this.route.params.subscribe(params=>{console.log(params)
+    if(params.id=='-'){
+    this.server.byGet("Travel").subscribe(data=>{
+      this.travels=new MatTableDataSource(data);
+      this.travels.sort=this.sort
+      console.log(this.travels);
+    });
+  }
+  else{
+    this.server.getTravelByGroupCode("Travel",params.id).subscribe(data=>{
+      this.travels=new MatTableDataSource(data);
+      this.travels.sort=this.sort    }
+      )
+  }
   });
   }
-  OnMouseOver(id:number)
-  {
-    this.server.getByParmater("Track",id).subscribe(data=> 
-      {
-        this.tracks=data;
-         console.log("succeed")
-      });  
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.travels.filter = filterValue.trim().toLowerCase();
   }
-  view(trackCode:number){
-    this.router.navigate(["/viewP",trackCode])
+  addTravel(){
+    this.router.navigate(['/addTravel']);
+    // alert('add travel')
+  }
+  updateTravel(travel:Travel){
+    console.log(travel);
+    this.router.navigate(["/editTravel/:id",travel]);
+
   }
 }
